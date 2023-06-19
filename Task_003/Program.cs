5421ffffbbece17edb2b489da3b18d5791856fe6
Shamil Magomedov <shmagomedov1997@mail.ru>
2023-06-19 20:16:34 +0300

    add task_003

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -2,3 +2,19 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+System.Console.WriteLine("Введите цифру: ");
+int dig = Convert.ToInt32 (Console.ReadLine());
+
+if (dig == 6 || dig == 7)
+{
+    System.Console.WriteLine("Да");
+}
+else if (dig > 7 || dig == 0)
+{
+    System.Console.WriteLine("Такого дня недели не сущестует");
+}
+else 
+{
+    System.Console.WriteLine("Нет");
+}
